351de3a15ef8a10decd3a9758773dc1f4be9c82a
dino89840 <dino89490@gmail.com>
2026-05-15 17:43:12 +0630

    Update media.js

--- functions/api/media.js
+++ functions/api/media.js
@@ -1,36 +1,39 @@
-import { AwsClient } from 'aws4fetch';
+import { AwsClient } from 'https://esm.sh/aws4fetch@1.0.17';
 
 export async function onRequest(context) {
   const requestUrl = new URL(context.request.url);
   const fileName = requestUrl.searchParams.get("file");
 
-  if (!fileName) {
-    return new Response("File name is missing", { status: 400 });
-  }
-
   const aws = new AwsClient({
     accessKeyId: context.env.WASABI_ACCESS_KEY,
     secretAccessKey: context.env.WASABI_SECRET_KEY,
     service: 's3',
-    region: 'ap-southeast-1' // အစ်ကို့ Wasabi Region အတိုင်း (လိုအပ်ရင် ပြင်ပေးပါ)
+    region: 'ap-southeast-1'
   });
 
-  const bucketName = "lugyi";
-  const wasabiUrl = `https://s3.ap-southeast-1.wasabisys.com/${bucketName}/${fileName}`;
+  const wasabiUrl = `https://s3.ap-southeast-1.wasabisys.com/lugyi/${fileName}`;
 
-  try {
-    // ၇ ရက်စာ (604800 စက္ကန့်) သက်တမ်းရှိတဲ့ Link ထုတ်မယ်
-    const signedRequest = await aws.sign(wasabiUrl, {
-      method: 'GET',
-      awsService: 's3',
-      signQuery: true,
-      expiresIn: 604800 
+  // ၁။ ဖိုင်ဆိုဒ်နဲ့ အမျိုးအစားကို သိရအောင် Wasabi ကို HEAD Request ပို့မယ်
+  const headRes = await aws.fetch(wasabiUrl, { method: 'HEAD' });
+
+  // ၂။ အကယ်၍ APK က File Size စစ်ဖို့ လှမ်းခေါ်တာဆိုရင် (HEAD request)
+  if (context.request.method === 'HEAD') {
+    return new Response(null, {
+      headers: {
+        'Content-Length': headRes.headers.get('content-length'),
+        'Content-Type': 'video/mp4',
+        'Accept-Ranges': 'bytes'
+      }
     });
-
-    // User ကို Wasabi လင့်ခ်အရှည်ကြီးဆီ Redirect လုပ်ပေးမယ်
-    return Response.redirect(signedRequest.url, 302);
-    
-  } catch (error) {
-    return new Response("Error generating link", { status: 500 });
   }
+
+  // ၃။ တကယ်ဒေါင်းဖို့အတွက် Redirect လုပ်မယ်
+  const signedRequest = await aws.sign(wasabiUrl, {
+    method: 'GET',
+    awsService: 's3',
+    signQuery: true,
+    expiresIn: 604800
+  });
+
+  return Response.redirect(signedRequest.url, 302);
 }
